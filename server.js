da67d506023cc7b62adde10bff6c1ee86daef632
Jai <105337050+jai2k15@users.noreply.github.com>
2023-02-08 12:01:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 const path = require("path");
 
-const port = 3000
+const port = 8080
 
 app.get('/', (req, res) => {
 //   res.send('Hello World this si live refresh!dfg ')

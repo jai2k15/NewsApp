c0b3fa6fb67e1b3a9c4a7903fe19c17d3808cafb
Jai <105337050+jai2k15@users.noreply.github.com>
2023-02-08 12:02:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const e = require('express');
 const express = require('express')
 const app = express()
 const path = require("path");
+const axios = require("axios");
 
 const port = 8080
 
@@ -11,6 +12,14 @@ app.get('/', (req, res) => {
     // return res.sendFile('public/index.html', );
     // res.sendFile("index.html", { root: __dirname})
     res.sendFile(path.join(__dirname,"/index.html"))
+
+})
+app.get('/api', async(req, res) => {
+  console.log(req._parsedUrl.query);
+  let url = "https://newsapi.org/v2/everything?"+req._parsedUrl.query;
+  let r = await axios(url);
+  let a = r.data;
+  res.json(a);
 
 })
 app.use(express.static('public'));
